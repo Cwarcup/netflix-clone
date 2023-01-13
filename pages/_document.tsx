f17b08d822c5bb51fc4b863b430b0ec087f84b54
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-13 14:13:05 -0800

    known styles for netflix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
