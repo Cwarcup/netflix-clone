b4e10deffdcd012de423ca0019e2f156ca0c5dfb
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-13 14:04:02 -0800

    add font with next/fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import { Roboto_Slab } from "@next/font/google"
+import type { AppProps } from "next/app"
+
+const roboto_slab = Roboto_Slab({
+  subsets: ["latin"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto_slab.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
