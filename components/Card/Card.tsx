f0c9896d3c13f061ef45178d0c27e0a0b480c917
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-14 09:23:07 -0800

    fallback image for card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useState } from "react"
 import styles from "./Card.module.css"
 
 // card should have 3 sizes: small, medium, large
@@ -20,8 +21,16 @@ const imgSizes = {
 }
 
 const Card = (props: CardProps) => {
-  const { imgURL, size, id, shouldScale } = props
+  const { imgURL, size = "medium", id, shouldScale } = props
 
+  const [imgSrc, setImgSrc] = useState(imgURL)
+
+  const handleOnError = () => {
+    console.log("Error loading image")
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+    )
+  }
   return (
     <div className={styles.container}>
       <div className={imgSizes[size]}>
@@ -30,6 +39,7 @@ const Card = (props: CardProps) => {
           alt="Picture of the author"
           fill
           className={styles.cardImg}
+          onError={handleOnError}
         />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         shouldScale={true}
       />
       <Card
-        imgURL="/static/breaking_bad_banner.jpeg"
+        imgURL="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
         size="small"
         id="3"
         shouldScale={true}
