1691082af8f66bfbcb3a5adc44d38a5e104755d8
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-16 10:29:41 -0800

    error handling for youtube api

--- lib/getYoutubeVideos.ts
+++ lib/getYoutubeVideos.ts
@@ -4,28 +4,42 @@ import type { CardType } from "@/types"
 // accepts a search term as a parameter
 // used in index.tsx on server side rendering
 const getYoutubeVideos = async (searchQuery: string): Promise<CardType[]> => {
-  const url = `https://youtube.googleapis.com/youtube/v3/search?q=${searchQuery}&key=${process.env.YOUTUBE_API_KEY}&part=snippet&maxResults=10&type=video&videoEmbeddable=true`
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
+  try {
+    const url = `https://youtube.googleapis.com/youtube/v3/search?q=${searchQuery}&key=${process.env.YOUTUBE_API_KEY}&part=snippet&maxResults=10&type=video&videoEmbeddable=true`
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
 
-  const data = await response.json()
+    const data = await response.json()
 
-  // mutate the data to match the CardType interface
-  const YoutubeMutatedData: CardType[] = data.items.map((item: any) => {
-    return {
-      id: item.id.videoId,
-      title: item.snippet.title,
-      description: item.snippet.description,
-      imgUrl: item.snippet.thumbnails.high.url,
-      publishedAt: item.snippet.publishedAt,
-    } as CardType
-  })
+    // mutate the data to match the CardType interface
+    const YoutubeMutatedData: CardType[] = data.items.map((item: any) => {
+      return {
+        id: item.id.videoId,
+        title: item.snippet.title,
+        description: item.snippet.description,
+        imgUrl: item.snippet.thumbnails.high.url,
+        publishedAt: item.snippet.publishedAt,
+      } as CardType
+    })
 
-  return YoutubeMutatedData
+    return YoutubeMutatedData
+  } catch (error) {
+    console.error(error)
+
+    return [
+      {
+        id: "0",
+        title: "Something went wrong fetching Youtube videos",
+        description: "Something went wrong fetching Youtube videos",
+        imgUrl: "https://i.imgur.com/0Z0Z0Z0.png",
+        publishedAt: new Date().toISOString(),
+      } as CardType,
+    ]
+  }
 }
 
 export default getYoutubeVideos
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "i.ytimg.com"],
+    domains: ["images.unsplash.com", "i.ytimg.com", "i.imgur.com"],
   },
 }
 
