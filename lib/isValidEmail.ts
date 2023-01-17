30b15f31805a59bbc0c7180e5c657a78e8e50b20
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-17 11:39:48 -0800

    error handling for login

--- lib/isValidEmail.ts
+++ lib/isValidEmail.ts
@@ -1,4 +1,4 @@
-export function isValidEmail(email: string): boolean {
+export function isValidEmail(email: string ): boolean {
   // Regular expression pattern for a valid email address
   const pattern =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
--- pages/login.tsx
+++ pages/login.tsx
@@ -16,20 +16,39 @@ const Login = (props: Props) => {
   // create a state for the email input
   const [email, setEmail] = useState<string | null>(null)
 
+  const [userMsg, setUserMsg] = useState<string | null>(null)
+
+  // if emailInput is being typed in, clear the user message
+  const handleEmailInputTyping = () => {
+    setUserMsg(null)
+  }
+
   // fn to handle login with email
   const handleLoginWithEmail = () => {
+    console.log("button clicked")
     // get the value from the email input
     const email = emailInputRef.current?.value || null
     // need to check if the email is valid
     // if not, show an error message
-    if (email && !isValidEmail(email)) {
-      alert("Please enter a valid email address")
+
+    if (!email) {
+      setUserMsg("Please enter an email address")
+      return
+    }
+
+    if (!isValidEmail(email)) {
+      console.log(isValidEmail(email))
+      setUserMsg("Please enter a valid email address")
       return
     }
 
     // set the email state
     setEmail(email)
+    // clear the user message
+    setUserMsg(null)
   }
+
+  console.log("email", email)
 
   return (
     <div className={styles.container}>
@@ -74,8 +93,11 @@ const Login = (props: Props) => {
               type="text"
               placeholder="Enter your email"
               ref={emailInputRef}
+              onChange={handleEmailInputTyping}
               aria-label="Email Address"
             />
+
+            {userMsg && <p className={styles.userMsg}>{userMsg}</p>}
 
             <button
               className={styles.loginBtn}
@@ -85,6 +107,24 @@ const Login = (props: Props) => {
             >
               Sign In
             </button>
+            {/* remember me input and text, is not functional */}
+            <div className={styles.rememberMeWrapper}>
+              <div className={styles.rememberMeInputWrapper}>
+                <input
+                  className={styles.rememberMeInput}
+                  type="checkbox"
+                  id="rememberMe"
+                  name="rememberMe"
+                  value="rememberMe"
+                />
+                <label className={styles.rememberMeLabel} htmlFor="rememberMe">
+                  Remember me
+                </label>
+              </div>
+              <Link href="/" className={styles.helpLink}>
+                Need help?
+              </Link>
+            </div>
           </form>
         </div>
       </main>
