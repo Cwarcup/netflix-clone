dbee405b1b466bbe1dfa72329f93c708e11d0c19
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-01-25 13:48:59 -0800

    styling changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({
   return (
     <div className={styles.container}>
       <Head>
-        <title>Netflix Clone</title>
+        <title>Netflix Clone</title> 
         <meta name="description" content="Created by Curtis Warcup" />
       </Head>
       <Navbar />
