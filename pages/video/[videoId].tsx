8e9753cf2e4ce577392f63f5f5ec8e34b5a695b9
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2024-01-06 10:19:47 -0800

    Update YouTube video embed URL

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -123,7 +123,7 @@ const VideoForId = ({ video }: Props) => {
             className={styles.videoPlayer}
             width="100%"
             height="360"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1&showinfo=0`}></iframe>
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}></iframe>
           <div
             id="video-gradient"
             className={styles.videoGradient}
